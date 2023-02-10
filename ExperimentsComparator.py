1af5d1ca863b24113056922311559c2499ab2cc4
Alessio Gaspar <profgrumpy@gmail.com>
2023-02-10 04:59:36 -0500

    Finishing up ExperimentComparator.
    Started on previous commit.
    Allows to save to binary files the data for runs.
    We can then open two such data sets and apply statistical tests on their difference.

--- ExperimentsComparator.py
+++ ExperimentsComparator.py
@@ -8,45 +8,52 @@ import numpy
 #f2_name = input('File #2 - name: ')
 #f1_name = sys.argv[1]
 #f2_name = sys.argv[2]
-root = 'Experiments Logs/'
-f1_name = root + 'OnePlusOneGA-with-recombination/stat3.pickle'
-f2_name = root + 'OnePlusOneGA-without-recombination/stat3.pickle'
+for i in range(4):
+    root = 'Experiments Logs/'
+    f1_name = root + 'OnePlusOneGA-with-recombination/'
+    f2_name = root + 'OnePlusOneGA-without-recombination/'
+    suffix = f'stat{i}.pickle'
+    f1_name += suffix
+    f2_name += suffix
 
-with open(f1_name, 'rb') as f:
-    series1 = pickle.load(f)
-with open(f2_name, 'rb') as f:
-    series2 = pickle.load(f)
+    with open(f1_name, 'rb') as f:
+        series1 = pickle.load(f)
+    with open(f2_name, 'rb') as f:
+        series2 = pickle.load(f)
 
 
-print('Series 1 -->', f1_name)
-print('Series 2 -->', f2_name)
-print("\tSeries 1 mean =", numpy.mean(series1))
-print("\tSeries 2 mean =", numpy.mean(series2))
+    print('-' * 18)
+    print(f'STATISTIC #{i}')
+    print('-' * 18)
+    print('\tSeries 1 -->', f1_name)
+    print('\tSeries 2 -->', f2_name)
+    print("\tSeries 1 mean =", numpy.mean(series1))
+    print("\tSeries 2 mean =", numpy.mean(series2))
 
-# Performing statistical test
-# see https://machinelearningmastery.com/statistical-hypothesis-tests-in-python-cheat-sheet/
+    # Performing statistical test
+    # see https://machinelearningmastery.com/statistical-hypothesis-tests-in-python-cheat-sheet/
 
-stat, p = shapiro(series1)
-print('\tShapiro: \t stat=%10.3f, p=%10.3f \t --> ' % (stat, p), end="")
-if p > 0.05:
-    print('Probably Gaussian')
-else:
-    print('Probably not Gaussian')
+    stat, p = shapiro(series1)
+    print('\tShapiro: \t stat=%10.3f, p=%10.3f \t --> ' % (stat, p), end="")
+    if p > 0.05:
+        print('Probably Gaussian')
+    else:
+        print('Probably not Gaussian')
 
-stat, p = ttest_ind(series1, series2)
-print('\tStudent T: \t stat=%10.3f, p=%10.3f \t --> ' % (stat, p), end="")
-if p > 0.05:
-    print('SAME distribution')
-else:
-    print('DIFFERENT distributions')
+    stat, p = ttest_ind(series1, series2)
+    print('\tStudent T: \t stat=%10.3f, p=%10.3f \t --> ' % (stat, p), end="")
+    if p > 0.05:
+        print('SAME distribution')
+    else:
+        print('DIFFERENT distributions')
 
-stat, p = mannwhitneyu(series1, series2)
-print("\tMann-Whitney: \t stat=%10.3f, p=%10.3f \t --> " % (stat, p), end="")
-if p > 0.05:
-    #print('H0 not rejected: samples are from SAME distribution')
-    print('SAME distribution')
-else:
-    #print('H0 rejected: samples are from DIFFERENT distribution')
-    print('DIFFERENT distribution')
+    stat, p = mannwhitneyu(series1, series2)
+    print("\tMann-Whitney: \t stat=%10.3f, p=%10.3f \t --> " % (stat, p), end="")
+    if p > 0.05:
+        #print('H0 not rejected: samples are from SAME distribution')
+        print('SAME distribution')
+    else:
+        #print('H0 rejected: samples are from DIFFERENT distribution')
+        print('DIFFERENT distribution')
 
 
