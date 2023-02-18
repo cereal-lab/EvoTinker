a7fdd46179d3200d056a472b569b02a91eb9cb56
Alessio Gaspar [alessio] <alessio@itn0.rc.usf.edu>
2023-02-18 08:32:46 -0500

    SLURM experiment. 400k iterations for 1+1 to see if it catches up to SSGA if given more resources.

--- evotinker.sh
+++ evotinker.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# SLURM Script to run experiments
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alessio@usf.edu
 #SBATCH --output=/home/a/alessio/evotinker/outputs/output.%j
 #SBATCH --partition=amd_2021
-#SBATCH --job-name=evotinker
-#SBATCH --time=10:00:00
+#SBATCH --job-name=evotnkr
+#SBATCH --time=96:00:00
 #SBATCH --mem=64000
 module purge
 module add apps/python/3.8.5
-cd runs 
+cd ~/evotinker/runs 
 #git clone git@github.com:profgrumpy/EvoTinker.git
 cd EvoTinker
 #pip install pipenv
