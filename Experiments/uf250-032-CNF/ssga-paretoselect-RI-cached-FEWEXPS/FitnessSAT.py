2064aa44453c0efa04c88c9fad65142ca4b8178d
Alessio Gaspar <profgrumpy@gmail.com>
2024-02-29 19:39:53 -0500

    Testing experiment.
    Running only 4 trials on multicore with ssga RI cached. When comparing with corresponding uf100 experiment, turned out that the same amount of resources also allows us to almost find the optimum. These SAT instances are supposed to be hard (uniform SAT + near transition threshold) but why can we solve a larger one without needing more resources?

---
+++ Experiments/uf250-032-CNF/ssga-paretoselect-RI-cached-FEWEXPS/FitnessSAT.py
@@ -0,0 +1,52 @@
+import random
+
+
+#91 clauses, 20 vars
+# file1 = open('uf20-04.cnf', 'r')
+# MAX_FITNESS = 91
+
+#430 clauses, 100 vars
+#file1 = open('uf100-04.cnf', 'r')
+#MAX_FITNESS = 430
+
+#1065 clauses, 250 vars
+file1 = open('uf250-032.cnf', 'r')
+MAX_FITNESS = 1065
+
+count = 0
+formula = []
+while True:
+    count += 1
+    line = file1.readline()
+    if not line:
+        break
+    if line[0] == '%':
+        break
+    if line[0] != 'c' and line[0] != 'p':
+        liner = []
+        #print("Line{}: {}".format(count, line.strip()))
+        liner = line.strip().split(' ')
+        #print(liner)
+        formula.append(liner[:-1])
+file1.close()
+
+
+def evaluate_formula(genotype: list):
+    evaluation = [None for _ in range(len(formula))]
+
+    for i in range(len(formula)):
+        clause = formula[i]
+        result = False
+        for t in range(len(clause)):
+            index = abs(int(clause[t])) - 1
+            value = True if genotype[index] == 1 else False
+            if int(clause[t]) < 0: 
+                value = not value
+            result = result or value
+        evaluation[i] = result
+
+    fitness = sum([1 if val == True else 0 for val in evaluation])
+    return fitness, evaluation
+
+if __name__ == '__main__':
+    pass
