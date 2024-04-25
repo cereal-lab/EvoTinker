0e6cfe00c0726e26688de2c7a6d48c002e1841bb
Alessio Gaspar <profgrumpy@gmail.com>
2024-04-25 03:54:50 -0400

    New version of evotinker.sh
    Allows to run SLURM stuff in current folder instead since we do not need anymore to work in the /work_bgfs/ file system.
    Also enables the long job qos.

--- evotinker.sh
+++ evotinker.sh
@@ -1,18 +1,28 @@
+
 #!/bin/bash
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alessio@usf.edu
-#SBATCH --output=/work_bgfs/a/alessio/evotinker2024/outputs/output.%j
+#SBATCH --qos=longrun
+#SBATCH --output=./output.%j
 #SBATCH --partition=amd_2021
-#SBATCH --job-name=evotnkr
-#SBATCH --time=96:00:00
+#SBATCH --job-name=100npsCC
+#SBATCH --time=400:00:00
 #SBATCH --mem=64000
+
+#/work_bgfs/a/alessio/evotinker2024/outputs/output.%j
+RUN_FOLDER=./
+#/work_bgfs/a/alessio/evotinker2024/runs
+
 module purge
 module add apps/python/3.8.5
-cd /work_bgfs/a/alessio/evotinker2024/runs
-rm -rf ./EvoTinker
-git clone https://github.com/cereal-lab/EvoTinker.git
-cd EvoTinker
-rm -f evotinker.sh
+# EvoTinker must be in the current folder of this script 
+#cp -r EvoTinker $RUN_FOLDER/
+#cd $RUN_FOLDER
+
+#rm -rf ./EvoTinker
+#git clone https://github.com/cereal-lab/EvoTinker.git
+#cd EvoTinker
+#rm -f evotinker.sh
 pip install pipenv
 export PYTHONPATH=$(pipenv run which python)
 #echo $PYTHONPATH
