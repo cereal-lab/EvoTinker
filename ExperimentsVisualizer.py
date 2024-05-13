c35fe6c37ed2884a702478daa92f378ff1abf18f
Alessio Gaspar <profgrumpy@gmail.com>
2024-05-13 00:22:55 -0400

    Allowing ExperimentVisualizer.py to plot a single file.

--- ExperimentsVisualizer.py
+++ ExperimentsVisualizer.py
@@ -3,30 +3,35 @@ import pickle
 import matplotlib.pyplot as plt
 
 file_core1 = sys.argv[1]
-file_core2 = sys.argv[2]
+if sys.arv[2]:
+    file_core2 = sys.argv[2]
 
 #root = 'Experiments Logs/'
 root = ''
 suffix = f''
 file_name1 = root + file_core1 + suffix
-file_name2 = root + file_core2 + suffix
+if sys.arv[2]:
+    file_name2 = root + file_core2 + suffix
 
 with open(file_name1, 'rb') as f1:
     file_series1 = pickle.load(f1)
 
-with open(file_name2, 'rb') as f2:
-    file_series2 = pickle.load(f2)
+if sys.arv[2]:
+    with open(file_name2, 'rb') as f2:
+        file_series2 = pickle.load(f2)
 
 x = [i for i in range(len(file_series1))]
 
 plt.plot(x, file_series1, label='SSGA')
-plt.plot(x, file_series2, label='RI-original')
+if sys.arv[2]:
+    plt.plot(x, file_series2, label='RI-original')
+
 #plt.title(f"Experiment 0a: Fitness vs. iterations")
 plt.xlabel("Iteration Number")
 plt.ylabel(f"Best Fitness (averaged)")
 #plt.ylim(80,92)
 #plt.xlim(49980, 50400)
-plt.legend(loc='lower right')#, labels=['SSGA','SSGA/RI'])
+plt.legend(loc='lower right')
 #plt.savefig('exp0b.png')
 plt.show()
 
